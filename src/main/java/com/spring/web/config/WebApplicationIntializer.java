2bad110f2090c4b0df3aedcaae80e8acf68beff2
Synergistisitit <synergisticit2020@gmail.com>
2018-12-20 14:15:55 -0800

    fix for session faqctory with connection pool!

--- src/main/java/com/spring/web/config/WebApplicationIntializer.java
+++ src/main/java/com/spring/web/config/WebApplicationIntializer.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.view.JstlView;
  * profile-servlet.xml
  * @author Nagendra
  *  This is acting as spring web application context
- *   
+ *  profile-servlet.xml 
  */
 @Configuration
 @EnableWebMvc
